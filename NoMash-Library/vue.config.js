243a47e7f265102a39ae77024b6ba44d81b7a268
Zehui Zhang <zzha0455@student.monash.edu>
2024-10-20 22:34:36 +1100

    Initial commit

--- NoMash-Library/vue.config.js
+++ NoMash-Library/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/fit5032-week12-ef/' : '/'
 }
